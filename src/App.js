a84666cc69ded8bf4bf2f920e7f101d2c9fc6e4f
Bassam Salah Zakaria <fbsam.tkrcl@gmail.com>
2021-04-09 07:12:32 +0200

    add ant button to home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "antd";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+          <Button type="primary">Primary Button</Button>
+          <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
